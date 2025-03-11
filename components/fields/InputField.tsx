a7f058d5b85f284d4553d19a4f03dd6495dd1b4d
SAEED.PANAHI <panahi.projects@gmail.com>
2025-03-12 01:10:07 +0330

    add input field

---
+++ components/fields/InputField.tsx
@@ -0,0 +1,31 @@
+import React from "react";
+
+interface InputFieldProps {
+  id: string;
+  label: string;
+  type: "text" | "number";
+  required?: boolean;
+}
+
+const InputField: React.FC<InputFieldProps> = ({
+  id,
+  label,
+  type,
+  required,
+}) => {
+  return (
+    <div className="mb-4">
+      <label htmlFor={id} className="block text-sm font-medium text-gray-700">
+        {label} {required && <span className="text-red-500">*</span>}
+      </label>
+      <input
+        id={id}
+        type={type}
+        required={required}
+        className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-blue-500 focus:ring-blue-500 p-2"
+      />
+    </div>
+  );
+};
+
+export default InputField;
