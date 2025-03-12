cd8f731c9494057e7d69c487516b5253a300555b
SAEED.PANAHI <panahi.projects@gmail.com>
2025-03-12 21:51:14 +0330

    submit button sticky in mobile screen

--- components/DynamicForm.tsx
+++ components/DynamicForm.tsx
@@ -23,18 +23,19 @@ const FormContent: React.FC<{ formData: any }> = ({ formData }) => {
   };
 
   return (
-    <form onSubmit={handleSubmit}>
+    <form onSubmit={handleSubmit} className="mb-12">
       {formData.fields.map((field: any) => (
         <DynamicFormField key={field.id} {...field} />
       ))}
-      {/* <ButtonField title="Submit Application" type="button" /> */}
-      <div className="flex justify-end">
-        <button
-          type="submit"
-          className="bg-primary rounded-md text-white py-2 px-6 mt-4"
-        >
-          Submit Application
-        </button>
+      <div className="flex justify-end mt-4">
+        <div className="bg-gray-200 sm:bg-transparent fixed bottom-0 left-0 right-0 py-4 sm:relative sm:mt-0 sm:bottom-auto sm:left-auto sm:right-auto lg:px-0 px-4">
+          <button
+            type="submit"
+            className="bg-primary rounded-md text-white py-2 px-6 w-full sm:w-auto sm:mx-0"
+          >
+            Submit Application
+          </button>
+        </div>
       </div>
     </form>
   );
